e2379e57bd8d73418dce53e412ad730404dc79e0
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-03-02 11:47:57 +0200

    Initial Commit

---
+++ UsingTheSwiftUIEnvironment/UsingTheSwiftUIEnvironmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingTheSwiftUIEnvironmentApp.swift
+//  UsingTheSwiftUIEnvironment
+//
+//  Created by Ramill Ibragimov on 02.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct UsingTheSwiftUIEnvironmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
